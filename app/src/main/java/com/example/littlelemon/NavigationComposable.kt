272b554f45f65dc899e47febabc2ece2cf2e5f91
Aniket tiwari <ani962854.at@gmail.com>
2023-05-21 19:47:25 +0530

    Added Navigation and Profile Screen

---
+++ app/src/main/java/com/example/littlelemon/NavigationComposable.kt
@@ -0,0 +1,36 @@
+package com.example.littlelemon
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.util.Log
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+
+@Composable
+fun NavigationComposable(
+    navController: NavHostController,
+    sharedPreferences: SharedPreferences,
+    applicationContext: Context
+){
+    Log.e("MainActivity","Inside Navigation Composable")
+    val startDestination:String = if(sharedPreferences.contains("firstName")&&sharedPreferences.contains("lastName")&&sharedPreferences.contains("email")){
+        Home.route
+    }else
+        OnBoarding.route
+
+
+    Log.e("MainActivity","Start Destination $startDestination ${sharedPreferences.contains("firstName")&&sharedPreferences.contains("lastName")&&sharedPreferences.contains("email")}")
+    NavHost(navController = navController, startDestination = startDestination ){
+        composable(OnBoarding.route){
+            OnBoarding(sharedPreferences.edit(),navController,applicationContext)
+        }
+        composable(Home.route){
+            Home(navController,sharedPreferences,applicationContext)
+        }
+        composable(Profile.route){
+            Profile(sharedPreferences,navController)
+        }
+    }
+}
