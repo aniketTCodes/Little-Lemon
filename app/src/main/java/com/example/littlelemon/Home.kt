729ff6730607da8b37f2568bddab027ce71b2267
Aniket tiwari <ani962854.at@gmail.com>
2023-05-22 17:54:30 +0530

    Added Database,api call for menu and implemented upper panel

--- app/src/main/java/com/example/littlelemon/Home.kt
+++ app/src/main/java/com/example/littlelemon/Home.kt
@@ -1,35 +1,151 @@
 package com.example.littlelemon
 
 import android.content.Context
-import android.content.SharedPreferences
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Home(
     navController: NavHostController,
-    sharedPreferences: SharedPreferences,
     applicationContext: Context
 ) {
-
-        Scaffold(topBar = { Header() }) {
+        var searchPhrase by rememberSaveable {
+            mutableStateOf("")
+        }
+        Scaffold(topBar = { HomeHeader(navController) }) {
             Column(modifier = Modifier.padding(it)) {
-                Image(
-                    painter = painterResource(id = R.drawable.profile),
-                    contentDescription = "Profile picture",
-                    modifier = Modifier.clickable { navController.navigate(Profile.route) })
+                Hero(searchPhrase, onChange = {searchPhrase=it})
             }
         }
     }
+
+
+@Composable
+fun HomeHeader(navController:NavHostController){
+    Card(modifier = Modifier.fillMaxWidth(),
+        colors = CardDefaults.cardColors(contentColor = Color.White, containerColor = Color.White),
+        shape = RoundedCornerShape(1.dp)
+    ) {
+        Row(
+            modifier=Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.End,
+            verticalAlignment = Alignment.CenterVertically
+
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription ="logo.png",
+                modifier= Modifier
+                    .size(width = 200.dp, height = 70.dp)
+                    .padding(end = 30.dp)
+            )
+            Image(
+                painter = painterResource(id = R.drawable.profile),
+                contentDescription = "Profile picture",
+                modifier = Modifier
+                    .clickable { navController.navigate(Profile.route) }
+                    .size(100.dp, 60.dp))
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Hero(
+    searchPhrase: String,
+    onChange:(String)->Unit
+) {
+    Surface(
+        color = LittleLemonColor.green,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Column(modifier=Modifier.padding(10.dp)) {
+            Row(
+                verticalAlignment = Alignment.Bottom,
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                modifier = Modifier.padding(bottom=15.dp)
+            ) {
+                Column() {
+                    Text(
+                        text = stringResource(id = R.string.title),
+                        style = MaterialTheme.typography.headlineSmall,
+                        color = LittleLemonColor.yellow,
+                        fontSize = 36.sp
+                    )
+                    Text(
+                        text = stringResource(id = R.string.location),
+                        modifier = Modifier.padding(bottom = 15.dp),
+                        style = MaterialTheme.typography.bodySmall,
+                        color = LittleLemonColor.yellow,
+                        fontSize = 24.sp,
+                    )
+                    Text(
+                        text = stringResource(id = R.string.description),
+                        color = Color.White,
+                        fontSize = 18.sp,
+                        modifier = Modifier
+                            .fillMaxWidth(0.60f)
+                            .padding(top = 15.dp)
+                    )
+                }
+                Image(
+                    painter = painterResource(id = R.drawable.heroimage),
+                    contentDescription = "hero Image",
+                    contentScale = ContentScale.FillBounds,
+                    modifier = Modifier
+                        .size(140.dp, 190.dp)
+                        .clip(RoundedCornerShape(10.dp))
+
+                )
+            }
+
+            TextField(
+                value = searchPhrase,
+                onValueChange = {onChange(it)},
+                modifier=Modifier.fillMaxWidth().padding(10.dp),
+                label = {
+                    Text(text = "Enter Search Phrase")
+                },
+                colors = TextFieldDefaults.textFieldColors(containerColor = Color.White)
+
+            )
+
+
+        }
+    }
+}
 
 
 
--- app/src/main/java/com/example/littlelemon/MainActivity.kt
+++ app/src/main/java/com/example/littlelemon/MainActivity.kt
@@ -7,12 +7,30 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.compose.rememberNavController
+import androidx.room.Room
 import com.example.littlelemon.ui.theme.LittleLemonTheme
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.engine.android.Android
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.http.ContentType
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
-    val sharedPreferences by lazy{getSharedPreferences("LittleLemon", MODE_PRIVATE)}
+    private val sharedPreferences by lazy {getSharedPreferences("LittleLemon", MODE_PRIVATE)}
+    private val client= HttpClient(Android){
+        install(ContentNegotiation){
+            json(contentType = ContentType("text","plain"))
+        }
+    }
+
+    private val database by lazy { Room.databaseBuilder(applicationContext,MenuDataBase::class.java,"database").build()}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -22,8 +40,29 @@ class MainActivity : ComponentActivity() {
                 NavigationComposable(navController = navController, sharedPreferences = sharedPreferences ,applicationContext)
             }
         }
+
+        lifecycleScope.launch(Dispatchers.IO){
+            if(database.menuItemDao().isEmpty()){
+                NetworkMenuItemsToDatabase(fetchMenu())
+            }
+        }
+    }
+
+
+    private suspend fun fetchMenu(): MenuNetworkData {
+        return client.get("https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/menu.json")
+            .body()
+    }
+
+    private suspend fun NetworkMenuItemsToDatabase(menuNetworkData: MenuNetworkData){
+        val menuItems:List<MenuItem> = menuNetworkData.menuItems.map { it.toMenuItems() }
+        menuItems.forEach {
+            database.menuItemDao().insertAll(it)
+        }
+
     }
 }
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
